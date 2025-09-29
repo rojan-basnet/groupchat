5df70dcc50a7f5354f8e4a6a898749755db4d9b0
Rojan Basnet <rojanbasnet072@gmail.com>
2025-09-29 11:20:20 +0545

    UI fixes

--- frontend/src/GroupChat.jsx
+++ frontend/src/GroupChat.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState,useRef } from "react"
 import { useLocation } from "react-router-dom"
 import './index.css'
 const GroupChat = () => {
@@ -9,9 +9,9 @@ const locate=useLocation()
 const user=locate.state?.user
 const [message,setMessage]=useState({user:user,message:""})
 const [count,setCount]=useState(0)
-
+const scrollRef=useRef()
 useEffect(()=>{
-  const ws=new WebSocket(`wss:${import.meta.env.VITE_SERVER_URL}`)
+  const ws=new WebSocket(`ws:${import.meta.env.VITE_SERVER_URL}`)
 
   ws.onopen=()=>{
     console.log("Client Connected")
@@ -34,34 +34,42 @@ useEffect(()=>{
   return ()=>ws.close()
 
 },[])
+useEffect(() => {
+  scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+}, [allmessages]);
 
 function handleMsgSend(){
   if(!isMsgBlank && socket && socket.readyState === WebSocket.OPEN){
     socket.send(JSON.stringify(message))
-    setMessage({...message,message:""})
+    setMessage({...message,message:""}) 
     setIsMsgBlank(true)
   }
+
 }
 
   return (
     <div className="mainGrpChatContainer">
-      <div className="Counter">Active users: {count}</div>
-      <div className="displayMsgs">
-        {
-          allmessages.map((ele,index)=>{
-            return <div key={index}><p>{ele.user}</p><h3>{ele.message}</h3></div>
-          })
-        }
+      <div className="secondary">
+        <div className="Counter">Active users: {count}</div>
+        <div className="displayMsgs">
+          {
+            allmessages.map((ele,index)=>{
+              
+              return <div key={index} className={ele.user === user?"UsersMessage" :"OtherMessages" }><p>{ele.user}</p><h3>{ele.message}</h3></div>
+            })
+          }
+        <div ref={scrollRef}></div>
+        </div>
       </div>
-      <div>
-        <form onSubmit={e=>e.preventDefault()}>
-          <input type="text"  value={message.message} 
-          onChange={e=>{setMessage({...message,message:e.target.value});
-          e.target.value.trim().length==0?setIsMsgBlank(true):setIsMsgBlank(false)}}/>
+        <div className="msgInput">
+          <form onSubmit={e=>e.preventDefault()}>
+            <input type="text"  value={message.message} placeholder="Messages" 
+            onChange={e=>{setMessage({...message,message:e.target.value});
+            e.target.value.trim().length==0?setIsMsgBlank(true):setIsMsgBlank(false)}}/>
 
-          <button onClick={handleMsgSend} style={{backgroundColor:isMsgBlank?"black":"ightblue"}}>Send</button>
-        </form>
-      </div>
+            <button onClick={handleMsgSend} style={{backgroundColor:isMsgBlank?"grey":"hsla(211, 100%, 51%, 1.00)",color:"black"}}>Send</button>
+          </form>
+        </div>
     </div>
   )
 }
